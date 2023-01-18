8dc0fb93a300a9d58717832331cdef2f808ab407
James <jkcarrolliv@gmail.com>
2023-01-18 16:49:59 -0500

    answer validated against correct answer. score computed and populated on page load and following submit. (JC)

--- index.js
+++ index.js
@@ -2,5 +2,32 @@ const num1 = Math.ceil(Math.random() * 10)
 const num2 = Math.ceil(Math.random() * 10)
 
 const questionEl = document.getElementById("question")
+const scoreEl = document.getElementById("score")
+const formEl = document.getElementById("form");
+const inputEl = document.getElementById("input")
+
+let score = +(localStorage.getItem("userScore") | 0);
+
+
+scoreEl.innerText = `Score: ${score}`
 questionEl.innerText = `What is ${num1} x ${num2}`
-console.log(num1, ' ', num2)
+
+const correctSolution = num1 * num2;
+
+formEl.addEventListener("submit", () =>{
+    //input value cast to number
+    const userSolution = +inputEl.value
+    if(correctSolution === userSolution){
+        //increase score
+        score++
+    } else {
+        score--
+    }
+    console.log(score);
+    updateLocalStorage()
+
+})
+
+function updateLocalStorage(){
+    localStorage.setItem("userScore", JSON.stringify(score))
+}
